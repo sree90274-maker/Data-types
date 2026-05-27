a774b849afbe1ea08ee3a072b6aae72ffe630d15
sree90274-maker <sree90274@gmail.com>
2026-05-27 11:54:09 +0530

    integers_datatypes

---
+++ integer.py
@@ -0,0 +1,7 @@
+Roll no = 3
+print(num)
+print(type(num))
+#Binary format
+roll no = 0b011 
+print(Roll no)
+print(type(bin))
---
+++ integer_dt.py
@@ -0,0 +1,25 @@
+ticket = 4
+print(ticket)
+print(type(ticket))
+
+#binary format
+emp_id = 0b100
+print(ticket)
+print(type(ticket))
+
+#Octal format
+roll_no = 0o12345
+print(ticket)
+print(type(ticket))
+
+#Hexa decimal format
+std_id = 0xABC
+print(std_id)
+print(type(std_id))
+
+#integer -> bin,oct,hex
+id = 40
+print("integer format 'id': ",id)
+print("Binary format 'id' : ",bin(id))
+print("Octal format 'id' : ",oct(id))
+print("hexadecimal format 'id' : ",hex(id))
