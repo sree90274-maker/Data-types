b74f2ea7a65bfc0d7661b8a2858ad144ddf40e9d
sree90274-maker <sree90274@gmail.com>
2026-05-27 12:19:44 +0530

    complex_datatypes

---
+++ complex_dt.py
@@ -0,0 +1,23 @@
+#complex format
+num = 3+4j
+print(num)
+print(type(num))
+
+num_1 = 5-2j
+print(num_1)
+print(type(num_1))
+
+num_2 = 6*3j
+print(num_2)
+print(type(num_2))
+
+#num_3 = 0+j	0 is a integer,j is variable that's why we get an a name error.interpreter expecting any value infront j
+#print(num_3)
+
+num_4 = 0+0j
+print(num_4)
+print(type(num_4))
+
+num_5 = 4/2j
+print(num_5)
+print(type(num_5))
