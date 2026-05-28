56e3ea8f9b0562cc989f46921b30213add3acfbe
sree90274-maker <sree90274@gmail.com>
2026-05-28 10:16:07 +0530

    datatypes

---
+++ boolean_dt.py
@@ -0,0 +1,17 @@
+#boolean format
+num_1 = 20
+num_2 = 40
+print(num_1>num_2)
+print(type(num_1>num_2))
+
+#frozen set
+students = frozenset(["ram", "sita", "durga"])
+marks = {80,70,60}
+print("students")
+print("marks")
+
+#string format
+name = "Durga sree"
+print(name)
+print(type(name))
+
