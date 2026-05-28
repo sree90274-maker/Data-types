4731b44b02f91339e63057c3009d330eecb39444
sree90274-maker <sree90274@gmail.com>
2026-05-28 09:36:31 +0530

    data_types

---
+++ tuple_dt.py
@@ -0,0 +1,11 @@
+#tuple format
+colors = ("red", "blue", "green")
+print(colors)
+
+#set format 
+numbers = {10, 20, 30, 40}
+print(numbers)
+
+#set format
+student = {"name": "Durga","age": 21,"course": "Python"}
+print(student)
