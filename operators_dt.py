17f16ceb0231d73002812bc535c0dddfce4d8798
sree90274-maker <sree90274@gmail.com>
2026-05-28 09:54:51 +0530

    operators

---
+++ operators_dt.py
@@ -0,0 +1,54 @@
+#operators
+#Arithmetic operators
+#addition
+num_1 = 25
+num_2 = 15
+result = num_1 + num_2
+print(result)
+
+#sutraction
+num_3 = 25
+num_4 = 15
+result = num_3 + num_4
+print(result)
+
+#multiplication
+price = 50
+quantity = 4
+result = price * quantity
+print(result)
+
+#division
+total = 100
+students = 4
+result = total/students
+print(result)
+
+#floor division
+a = 10
+b = 20
+result = 10//20
+print(a//b)
+
+#modulus
+apples = 10
+persons = 2
+print(apples%persons)
+#modulus
+roll_no = 17
+sections = 5
+section = roll_no%sections
+print(section)
+
+#exponential
+x = 3
+y = 4
+result = x**y
+print(result)
+
+
+
+
+
+
+
