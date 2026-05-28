6581444ab1402ba02681c009ffe5baedd075f6e2
sree90274-maker <sree90274@gmail.com>
2026-05-28 12:57:12 +0530

    mapping

---
+++ mapping_dt.py
@@ -0,0 +1,22 @@
+#Mapping operators
+student = {"name":"durga","age":20}
+print(student)
+ 
+#[] used to get value from the dictionary
+student = {"name":"durga","age":20}
+print(student["name"])
+
+#(=) used to ADD 0r UPDATE the value
+student = {"name":"durga"}
+student["age"]=30
+print(student)
+
+
+
+
+
+
+
+
+
+
