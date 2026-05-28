deab0136570b8cf6057c9306ae3c5f3ca3465f33
sree90274-maker <sree90274@gmail.com>
2026-05-28 11:20:04 +0530

    comparision_operators

---
+++ comparision_dt.py
@@ -0,0 +1,43 @@
+#comparision operators
+a = 12
+b = 4
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a>=b)
+
+#Equal to 
+student1_marks = 80
+student2_marks = 80
+print(student1_marks == student2_marks)
+
+#NOT EQUAL TO
+x = 10
+y = 20
+print(10!=20)
+
+#GREATER THAN
+num1 = 30
+num2 = 10
+print(30>10)
+
+#LESS THAN 
+num3 = 10
+num4 = 20
+print(num3<num4)
+
+#less than equal to
+a = 20
+b = 30
+print(a<=b)
+
+#Greater than equal to
+class1_students = 40
+class2_students = 90
+print(90>=40)
+
+
+
+
