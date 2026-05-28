77b34af8c5f377deb3971becdcb005a5bdb05390
sree90274-maker <sree90274@gmail.com>
2026-05-28 10:59:27 +0530

    bitwise_operators

---
+++ bitwise_dt.py
@@ -0,0 +1,42 @@
+#bitwise operators
+a = 6
+b = 3
+print(6&3)
+print(6|3)
+print(6^3)
+print(~3)
+print(6<<3)
+print(6>>3)
+
+#And operator 
+a = 6
+b = 3
+print(6&3)   #0110
+             #0010
+#OR operator
+number_1 = 7
+number_2 = 4
+print(7|4)     #0111
+               #0100
+
+#XOR operator
+num_3 = 8
+num_4 = 6
+print(8^6)     #1000
+               #0110
+
+#NOT operator
+x = ~6
+print(~6)     # by using formula -(n+1)
+
+#left shift
+a = 12
+b = 2
+print(12<<2)
+
+#right shift
+x = 16
+print(16>>2)
+
+
+
